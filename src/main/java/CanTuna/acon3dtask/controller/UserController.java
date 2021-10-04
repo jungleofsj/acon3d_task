6ce675634f249d43d2e8674765ef345cff53a020
CanTuna <jungleofsj@naver.com>
2021-10-04 22:36:10 +0900

    - UserController 구현  - Service loginUser TEST  - TABLE 생성, TABLE 생성 sql : acon3dtask/sql/ddl.sql

--- src/main/java/CanTuna/acon3dtask/controller/UserController.java
+++ src/main/java/CanTuna/acon3dtask/controller/UserController.java
@@ -2,7 +2,14 @@ package CanTuna.acon3dtask.controller;
 
 import CanTuna.acon3dtask.domain.User;
 import CanTuna.acon3dtask.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import java.util.List;
@@ -27,6 +34,42 @@ public class UserController {
     * uri : GET /users/all
     * */
 
+
+    @GetMapping("/users/new")
+    public String createForm() {
+        return "/createUserForm";
+    }
+
+    @PostMapping("/users/new")
+    public ResponseEntity createUser(@RequestBody User user) {
+        if (userService.joinUser(user) == -1L) {
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/login")
+    public String loginForm() {
+        return "/login";
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity validUser(@RequestBody User user) {
+        Long userType = userService.logInUser(user);
+
+        if (userType == null) {
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(userType, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping("/users/all")
+    public List<User> list(Model model) {
+        List<User> users = userService.findUserAll();
+        model.addAttribute("users", users);
+        return users;
+    }
 
 
 }
--- src/test/java/CanTuna/acon3dtask/service/UserServiceTest.java
+++ src/test/java/CanTuna/acon3dtask/service/UserServiceTest.java
@@ -19,22 +19,57 @@ class UserServiceTest {
     @Autowired
     UserRepository userRepository;
 
+
+/*        User testCreator = new User();
+        testCreator.setName("creator1");
+        testCreator.setType(User.USER_TYPE_CREATOR);
+
+        User testEditor = new User();
+        testEditor.setName("editor1");
+        testEditor.setType(User.USER_TYPE_EDITOR);
+
+        User testCustomer = new User();
+        testCustomer.setName("customer1");
+        testCustomer.setType(User.USER_TYPE_CUSTOMER);
+*/
+
+
     @Test
     void joinUser() {
+        //given
         User testCreator = new User();
         testCreator.setName("creator1");
         testCreator.setType(User.USER_TYPE_CREATOR);
 
+        //when
         Long savedId = userService.joinUser(testCreator);
         System.out.println(savedId);
         User findUser = userService.findUserById(savedId).get();
 
+        //then
         Assertions.assertThat(testCreator.getName()).isEqualTo(findUser.getName());
         System.out.println("USERID" + findUser.getId());
     }
 
     @Test
     void logInUser() {
+//        public Long logInUser(User user) {
+//            return userRepository.authUser(user);
+//        }
+
+        //given
+        User testEditor = new User();
+        testEditor.setName("editor1");
+        testEditor.setType(User.USER_TYPE_EDITOR);
+
+        userService.joinUser(testEditor);
+
+        User loginUser = new User();
+        loginUser.setName("editor1");
+        loginUser.setType(User.USER_TYPE_EDITOR);
+
+        Long logIn = userService.logInUser(loginUser);
+        Assertions.assertThat(logIn).isEqualTo(testEditor.getType());
     }
 
     @Test
