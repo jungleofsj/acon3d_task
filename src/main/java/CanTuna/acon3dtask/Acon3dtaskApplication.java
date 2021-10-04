d583a9d90af88de0c14e29725dda4f6063564660
CanTuna <jungleofsj@naver.com>
2021-10-04 20:33:56 +0900

    Initial commit

---
+++ src/main/java/CanTuna/acon3dtask/Acon3dtaskApplication.java
@@ -0,0 +1,13 @@
+package CanTuna.acon3dtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Acon3dtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Acon3dtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CanTuna/acon3dtask/Acon3dtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package CanTuna.acon3dtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Acon3dtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
